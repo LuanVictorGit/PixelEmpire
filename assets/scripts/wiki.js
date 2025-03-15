aa8dea8de4abde4fc33c1d06c30d1e5e1c50cada
LuanVictorGit <luanvictorchagas2015@gmail.com>
2025-03-15 12:15:47 -0300

    updated wiki

--- assets/scripts/wiki.js
+++ assets/scripts/wiki.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
 
-    document.addEventListener("click", async (e) => { //criando evento de selecionar qual botão da wiki.
+    document.addEventListener("click", async (e) => { // criando evento de selecionar qual botão da wiki e exibir o conteúdo desejado de cada botão.
         if (e.target.classList.contains("selectedButtonWiki")) {
             e.preventDefault();
             return;
@@ -11,6 +11,31 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
         let newTarget = e.target;
         newTarget.classList.add("selectedButtonWiki");
+
+        let lastContainer = document.getElementsByClassName("containerSelectedWiki")[0];
+        lastContainer.style.display = "none"
+        lastContainer.classList.remove("containerSelectedWiki");
+
+        let newContainer;
+        switch (newTarget.id) {
+            case "breedWiki":
+                newContainer = document.getElementById("containerBreeds");
+                break;
+            case "terrenosWiki":
+                newContainer = document.getElementById("containerTerrenos");
+                break;
+            case "caixasWiki":
+                newContainer = document.getElementById("containerCaixas");
+                break;
+            case "pokeluckysWiki":
+                newContainer = document.getElementById("containerPokeluckys");
+                break;
+            default:
+                break;
+        }
+        newContainer.classList.add("containerSelectedWiki");
+        newContainer.style.display = "unset";
+
     })
 
 });
